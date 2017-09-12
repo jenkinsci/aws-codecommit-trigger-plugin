467a9ef062ce3bb099f2b0f8703954b2b62b93de
phuonghqh <phuonghqh@gmail.com>
2017-09-12 23:10:57 +0700

    Fix findbug issue Nonnull

--- hpi/src/main/java/com/ribose/jenkins/plugin/awscodecommittrigger/model/SQSQueueProviderImpl.java
+++ hpi/src/main/java/com/ribose/jenkins/plugin/awscodecommittrigger/model/SQSQueueProviderImpl.java
@@ -6,6 +6,7 @@ import com.ribose.jenkins.plugin.awscodecommittrigger.interfaces.SQSQueue;
 import com.ribose.jenkins.plugin.awscodecommittrigger.interfaces.SQSQueueProvider;
 import jenkins.model.Jenkins;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 
@@ -13,13 +14,13 @@ public class SQSQueueProviderImpl implements SQSQueueProvider {
 
     @Override
     public List<? extends SQSQueue> getSqsQueues() {
-        final SQSTrigger.DescriptorImpl descriptor = (SQSTrigger.DescriptorImpl) Jenkins.getActiveInstance().getDescriptor(SQSTrigger.class);//SQSTrigger.DescriptorImpl.get();
+        @Nonnull final SQSTrigger.DescriptorImpl descriptor = (SQSTrigger.DescriptorImpl) Jenkins.getActiveInstance().getDescriptor(SQSTrigger.class);//SQSTrigger.DescriptorImpl.get();
         return descriptor.getSqsQueues();
     }
 
     @Override
     public SQSQueue getSqsQueue(final String uuid) {
-        final SQSTrigger.DescriptorImpl descriptor = (SQSTrigger.DescriptorImpl) Jenkins.getActiveInstance().getDescriptor(SQSTrigger.class);// SQSTrigger.DescriptorImpl.get();
+        @Nonnull final SQSTrigger.DescriptorImpl descriptor = (SQSTrigger.DescriptorImpl) Jenkins.getActiveInstance().getDescriptor(SQSTrigger.class);// SQSTrigger.DescriptorImpl.get();
         return descriptor.getSqsQueue(uuid);
     }
 }
